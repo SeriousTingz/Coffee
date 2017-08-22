af7bb0057b71277cface39c66df76ed9f7df1aee
SeriousTingz <Lakhanpar.Abdul@gmail.com>
2017-08-23 00:16:03 +0100

    Initial commit

---
+++ app/src/main/java/com/example/me/coffee/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.me.coffee;
+
+import android.icu.text.NumberFormat;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    int quanitity = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void increment(View view) {
+        quanitity = quanitity + 1;
+        display(quanitity);
+    }
+
+    public void decrement(View view) {
+        quanitity = quanitity - 1;
+        display(quanitity);
+    }
+
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+}
